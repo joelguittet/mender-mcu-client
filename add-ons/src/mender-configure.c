f5acfa0994e47f704b4b312867d368d11112d5ac
Joel Guittet <joelguittet@gmail.com>
2024-05-14 00:28:17 +0200

    add-ons: configure: update following integration of modules

--- add-ons/src/mender-configure.c
+++ add-ons/src/mender-configure.c
@@ -26,6 +26,7 @@
  */
 
 #include "mender-api.h"
+#include "mender-client.h"
 #include "mender-configure.h"
 #include "mender-log.h"
 #include "mender-rtos.h"
@@ -67,10 +68,28 @@ static void *mender_configure_work_handle = NULL;
  */
 static mender_err_t mender_configure_work_function(void);
 
+/**
+ * @brief Callback function to be invoked to perform the treatment of the data from the artifact type "mender-configure"
+ * @param id ID of the deployment
+ * @param artifact name Artifact name
+ * @param type Type from header-info payloads
+ * @param meta_data Meta-data from header tarball
+ * @param filename Artifact filename
+ * @param size Artifact file size
+ * @param data Artifact data
+ * @param index Artifact data index
+ * @param length Artifact data length
+ * @return MENDER_OK if the function succeeds, error code if an error occured
+ */
+static mender_err_t mender_configure_download_artifact_callback(
+    char *id, char *artifact_name, char *type, cJSON *meta_data, char *filename, size_t size, void *data, size_t index, size_t length);
+
 mender_err_t
 mender_configure_init(mender_configure_config_t *config, mender_configure_callbacks_t *callbacks) {
 
     assert(NULL != config);
+    char *       device_config      = NULL;
+    cJSON *      json_device_config = NULL;
     mender_err_t ret;
 
     /* Save configuration */
@@ -86,28 +105,40 @@ mender_configure_init(mender_configure_config_t *config, mender_configure_callba
     /* Create configure mutex */
     if (MENDER_OK != (ret = mender_rtos_mutex_create(&mender_configure_mutex))) {
         mender_log_error("Unable to create configure mutex");
-        return ret;
+        goto END;
     }
 
 #ifdef CONFIG_MENDER_CLIENT_CONFIGURE_STORAGE
 
     /* Initialize configuration from storage (if it is not empty) */
-    char *device_config = NULL;
     if (MENDER_OK != (ret = mender_storage_get_device_config(&device_config))) {
         if (MENDER_NOT_FOUND != ret) {
             mender_log_error("Unable to get device configuration");
-            return ret;
+            goto END;
         }
     }
 
     /* Set device configuration */
     if (NULL != device_config) {
-        if (MENDER_OK != (ret = mender_utils_keystore_from_string(&mender_configure_keystore, device_config))) {
+        if (NULL == (json_device_config = cJSON_Parse(device_config))) {
             mender_log_error("Unable to set device configuration");
-            free(device_config);
-            return ret;
+            ret = MENDER_FAIL;
+            goto END;
         }
-        free(device_config);
+        if (MENDER_OK != (ret = mender_utils_keystore_from_json(&mender_configure_keystore, cJSON_GetObjectItemCaseSensitive(json_device_config, "config")))) {
+            mender_log_error("Unable to set device configuration");
+            goto END;
+        }
+    }
+
+    /* Register mender-configure artifact type */
+    if (MENDER_OK
+        != (ret = mender_client_register_artifact_type("mender-configure",
+                                                       &mender_configure_download_artifact_callback,
+                                                       true,
+                                                       cJSON_GetStringValue(cJSON_GetObjectItemCaseSensitive(json_device_config, "artifact_name"))))) {
+        mender_log_error("Unable to register 'mender-configure' artifact type");
+        goto END;
     }
 
 #endif /* CONFIG_MENDER_CLIENT_CONFIGURE_STORAGE */
@@ -119,7 +150,17 @@ mender_configure_init(mender_configure_config_t *config, mender_configure_callba
     configure_work_params.name     = "mender_configure";
     if (MENDER_OK != (ret = mender_rtos_work_create(&configure_work_params, &mender_configure_work_handle))) {
         mender_log_error("Unable to create configure work");
-        return ret;
+        goto END;
+    }
+
+END:
+
+    /* Release memory */
+    if (NULL != device_config) {
+        free(device_config);
+    }
+    if (NULL != json_device_config) {
+        cJSON_Delete(json_device_config);
     }
 
     return ret;
@@ -168,6 +209,9 @@ END:
 mender_err_t
 mender_configure_set(mender_keystore_t *configuration) {
 
+    cJSON *      json_device_config = NULL;
+    cJSON *      json_config        = NULL;
+    char *       device_config      = NULL;
     mender_err_t ret;
 
     /* Take mutex used to protect access to the configuration key-store */
@@ -191,21 +235,42 @@ mender_configure_set(mender_keystore_t *configuration) {
 #ifdef CONFIG_MENDER_CLIENT_CONFIGURE_STORAGE
 
     /* Save the device configuration */
-    char *device_config = NULL;
-    if (MENDER_OK != (ret = mender_utils_keystore_to_string(mender_configure_keystore, &device_config))) {
+    if (NULL == (json_device_config = cJSON_CreateObject())) {
+        mender_log_error("Unable to allocate memory");
+        ret = MENDER_FAIL;
+        goto END;
+    }
+    if (MENDER_OK != (ret = mender_utils_keystore_to_json(mender_configure_keystore, &json_config))) {
         mender_log_error("Unable to format configuration");
         goto END;
     }
-    if (NULL != device_config) {
-        if (MENDER_OK != (ret = mender_storage_set_device_config(device_config))) {
-            mender_log_error("Unable to record configuration");
-        }
-        free(device_config);
+    if (NULL == json_config) {
+        mender_log_error("Unable to format configuration");
+        ret = MENDER_FAIL;
+        goto END;
+    }
+    cJSON_AddItemToObject(json_device_config, "config", json_config);
+    if (NULL == (device_config = cJSON_PrintUnformatted(json_device_config))) {
+        mender_log_error("Unable to allocate memory");
+        ret = MENDER_FAIL;
+        goto END;
+    }
+    if (MENDER_OK != (ret = mender_storage_set_device_config(device_config))) {
+        mender_log_error("Unable to record configuration");
+        goto END;
     }
 
 #endif /* CONFIG_MENDER_CLIENT_CONFIGURE_STORAGE */
 
 END:
+
+    /* Release memory */
+    if (NULL != json_device_config) {
+        cJSON_Delete(json_device_config);
+    }
+    if (NULL != device_config) {
+        free(device_config);
+    }
 
     /* Release mutex used to protect access to the configuration key-store */
     mender_rtos_mutex_give(mender_configure_mutex);
@@ -313,5 +378,65 @@ END:
 
     return ret;
 }
+
+#ifdef CONFIG_MENDER_CLIENT_CONFIGURE_STORAGE
+
+static mender_err_t
+mender_configure_download_artifact_callback(
+    char *id, char *artifact_name, char *type, cJSON *meta_data, char *filename, size_t size, void *data, size_t index, size_t length) {
+
+    (void)id;
+    (void)type;
+    (void)filename;
+    (void)size;
+    (void)data;
+    (void)index;
+    (void)length;
+    cJSON *      json_device_config = NULL;
+    char *       device_config      = NULL;
+    mender_err_t ret                = MENDER_OK;
+
+    /* Check meta-data */
+    if (NULL != meta_data) {
+
+        /* Save the device configuration */
+        if (NULL == (json_device_config = cJSON_CreateObject())) {
+            mender_log_error("Unable to allocate memory");
+            ret = MENDER_FAIL;
+            goto END;
+        }
+        cJSON_AddStringToObject(json_device_config, "artifact_name", artifact_name);
+        cJSON_AddItemToObject(json_device_config, "config", meta_data);
+        if (NULL == (device_config = cJSON_PrintUnformatted(json_device_config))) {
+            mender_log_error("Unable to allocate memory");
+            ret = MENDER_FAIL;
+            goto END;
+        }
+        if (MENDER_OK != (ret = mender_storage_set_device_config(device_config))) {
+            mender_log_error("Unable to record configuration");
+            goto END;
+        }
+
+    } else {
+
+        /* Delete the configuration */
+        if (MENDER_OK != (ret = mender_storage_delete_device_config())) {
+            mender_log_error("Unable to delete configuration");
+        }
+    }
+
+END:
+
+    /* Release memory */
+    if (NULL != json_device_config) {
+        cJSON_Delete(json_device_config);
+    }
+    if (NULL != device_config) {
+        free(device_config);
+    }
+    return ret;
+}
+
+#endif /* CONFIG_MENDER_CLIENT_CONFIGURE_STORAGE */
 
 #endif /* CONFIG_MENDER_CLIENT_ADD_ON_CONFIGURE */
