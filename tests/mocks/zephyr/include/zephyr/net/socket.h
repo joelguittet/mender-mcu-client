ac69293e5224cf309897fd90edbc4815c805c4b2
Joel Guittet <joelguittet@gmail.com>
2024-05-22 22:02:03 +0200

    tests: mocks: zephyr: update socket mocks

--- tests/mocks/zephyr/include/zephyr/net/socket.h
+++ tests/mocks/zephyr/include/zephyr/net/socket.h
@@ -9,7 +9,7 @@
 #define TLS_HOSTNAME     2
 #define TLS_PEER_VERIFY  5
 
-struct addrinfo {
+struct zsock_addrinfo {
     int              ai_family;
     int              ai_socktype;
     int              ai_protocol;
@@ -17,11 +17,11 @@ struct addrinfo {
     struct sockaddr *ai_addr;
 };
 
-int  socket(int family, int type, int proto);
-int  connect(int sock, const struct sockaddr *addr, socklen_t addrlen);
-int  setsockopt(int sock, int level, int optname, const void *optval, socklen_t optlen);
-int  getaddrinfo(const char *host, const char *service, const struct addrinfo *hints, struct addrinfo **res);
-void freeaddrinfo(struct addrinfo *ai);
-int  close(int sock);
+int  zsock_socket(int family, int type, int proto);
+int  zsock_connect(int sock, const struct sockaddr *addr, socklen_t addrlen);
+int  zsock_setsockopt(int sock, int level, int optname, const void *optval, socklen_t optlen);
+int  zsock_getaddrinfo(const char *host, const char *service, const struct zsock_addrinfo *hints, struct zsock_addrinfo **res);
+void zsock_freeaddrinfo(struct zsock_addrinfo *ai);
+int  zsock_close(int sock);
 
 #endif /* __SOCKET_H__ */
