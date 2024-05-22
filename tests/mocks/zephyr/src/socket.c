23036a77a293c6bab363cab69bd9603dc10de810
Joel Guittet <joelguittet@gmail.com>
2024-05-22 22:02:03 +0200

    tests: mocks: zephyr: update socket mocks

--- tests/mocks/zephyr/src/socket.c
+++ tests/mocks/zephyr/src/socket.c
@@ -1,30 +1,30 @@
 #include <zephyr/net/socket.h>
 
 int
-socket(int family, int type, int proto) {
+zsock_socket(int family, int type, int proto) {
     return 0;
 }
 
 int
-connect(int sock, const struct sockaddr *addr, socklen_t addrlen) {
+zsock_connect(int sock, const struct sockaddr *addr, socklen_t addrlen) {
     return 0;
 }
 
 int
-setsockopt(int sock, int level, int optname, const void *optval, socklen_t optlen) {
+zsock_setsockopt(int sock, int level, int optname, const void *optval, socklen_t optlen) {
     return 0;
 }
 
 int
-getaddrinfo(const char *host, const char *service, const struct addrinfo *hints, struct addrinfo **res) {
+zsock_getaddrinfo(const char *host, const char *service, const struct zsock_addrinfo *hints, struct zsock_addrinfo **res) {
     return 0;
 }
 
 void
-freeaddrinfo(struct addrinfo *ai) {
+zsock_freeaddrinfo(struct zsock_addrinfo *ai) {
 }
 
 int
-close(int sock) {
+zsock_close(int sock) {
     return 0;
 }
