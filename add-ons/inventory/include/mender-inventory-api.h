8385b7c012ce22a1c8b35319789cdc65e5c02507
Joel Guittet <joelguittet@gmail.com>
2024-08-15 12:53:00 +0200

    add-ons: inventory: moving add-on and related apis

---
+++ add-ons/inventory/include/mender-inventory-api.h
@@ -0,0 +1,46 @@
+/**
+ * @file      mender-inventory-api.h
+ * @brief     Mender MCU inventory add-on API implementation
+ *
+ * Copyright joelguittet and mender-mcu-client contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __MENDER_INVENTORY_API_H__
+#define __MENDER_INVENTORY_API_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#include "mender-utils.h"
+
+#ifdef CONFIG_MENDER_CLIENT_ADD_ON_INVENTORY
+
+/**
+ * @brief Publish inventory data of the device to the mender-server
+ * @param artifact_name Artifact name
+ * @param device_type Device type
+ * @param inventory Mender inventory key/value pairs table, must end with a NULL/NULL element, NULL if not defined
+ * @return MENDER_OK if the function succeeds, error code otherwise
+ */
+mender_err_t mender_inventory_api_publish_inventory_data(char *artifact_name, char *device_type, mender_keystore_t *inventory);
+
+#endif /* CONFIG_MENDER_CLIENT_ADD_ON_INVENTORY */
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* __MENDER_INVENTORY_API_H__ */
