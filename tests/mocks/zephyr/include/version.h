9b7aacf7954b476bffa5976ede7ff413bc6896df
Joel Guittet <joelguittet@gmail.com>
2025-07-16 22:54:47 +0200

    tests: mocks: zephyr: add ZEPHYR_VERSION definition

--- tests/mocks/zephyr/include/version.h
+++ tests/mocks/zephyr/include/version.h
@@ -2,5 +2,7 @@
 #define __VERSION_H__
 
 #define KERNEL_VERSION_STRING "x.y.z"
+#define ZEPHYR_VERSION(a, b, c) (((a) << 16) + ((b) << 8) + (c))
+#define ZEPHYR_VERSION_CODE ZEPHYR_VERSION(9, 9, 9)
 
 #endif /* __VERSION_H__ */
