6acf077b06ef10fc705881404d06bc8aced4a760
Joel Guittet <joelguittet@gmail.com>
2025-08-23 23:28:22 +0200

    platform: storage: zephyr: nvs: remove redundant configuration option

--- tests/mocks/zephyr/include/zephyr/storage/flash_map.h
+++ tests/mocks/zephyr/include/zephyr/storage/flash_map.h
@@ -1,7 +1,8 @@
 #ifndef __FLASH_MAP_H__
 #define __FLASH_MAP_H__
 
-#define FIXED_PARTITION_OFFSET(label) 0
 #define FIXED_PARTITION_DEVICE(label) NULL
+#define FIXED_PARTITION_OFFSET(label) 0
+#define FIXED_PARTITION_SIZE(label)   0
 
 #endif /* __FLASH_MAP_H__ */
