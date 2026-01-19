fd2a134fa6f0f7313fe33e15c2952d67134e7db5
Joel Guittet <joelguittet@gmail.com>
2026-01-19 09:45:58 +0100

    platform: net: zephyr: improve usage of getaddrinfo to establish connection

--- tests/mocks/zephyr/include/zephyr/net/socket.h
+++ tests/mocks/zephyr/include/zephyr/net/socket.h
@@ -9,18 +9,26 @@
 #define TLS_HOSTNAME     2
 #define TLS_PEER_VERIFY  5
 
+#define ZSOCK_NI_MAXHOST     64
+#define ZSOCK_NI_NUMERICHOST 1
+
 struct zsock_addrinfo {
-    int              ai_family;
-    int              ai_socktype;
-    int              ai_protocol;
-    socklen_t        ai_addrlen;
-    struct sockaddr *ai_addr;
+    struct zsock_addrinfo *ai_next;
+    int                    ai_flags;
+    int                    ai_family;
+    int                    ai_socktype;
+    int                    ai_protocol;
+    int                    ai_eflags;
+    socklen_t              ai_addrlen;
+    struct sockaddr       *ai_addr;
+    char                  *ai_canonname;
 };
 
 int  zsock_socket(int family, int type, int proto);
 int  zsock_connect(int sock, const struct sockaddr *addr, socklen_t addrlen);
 int  zsock_setsockopt(int sock, int level, int optname, const void *optval, socklen_t optlen);
 int  zsock_getaddrinfo(const char *host, const char *service, const struct zsock_addrinfo *hints, struct zsock_addrinfo **res);
+int zsock_getnameinfo(const struct net_sockaddr *addr, net_socklen_t addrlen, char *host, net_socklen_t hostlen, char *serv, net_socklen_t servlen, int flags);
 void zsock_freeaddrinfo(struct zsock_addrinfo *ai);
 int  zsock_close(int sock);
 
