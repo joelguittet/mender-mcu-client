2fabc8adae9d99feafc082a34af20289dc2a795c
Joel Guittet <joelguittet@gmail.com>
2026-04-21 22:07:35 +0200

    platform: tls: psa crypto: support volatile lifetime keys

--- tests/mocks/tf-m/include/psa/crypto_values.h
+++ tests/mocks/tf-m/include/psa/crypto_values.h
@@ -9,9 +9,9 @@
 
 #define PSA_ALG_SHA_256 ((psa_algorithm_t) 0x02000009)
 #define PSA_ALG_HASH_MASK ((psa_algorithm_t) 0x000000ff)
-#define PSA_ALG_DETERMINISTIC_ECDSA_BASE ((psa_algorithm_t) 0x06000700)
-#define PSA_ALG_DETERMINISTIC_ECDSA(hash_alg)                           \
-    (PSA_ALG_DETERMINISTIC_ECDSA_BASE | ((hash_alg) & PSA_ALG_HASH_MASK))
+#define PSA_ALG_ECDSA_BASE ((psa_algorithm_t) 0x06000600)
+#define PSA_ALG_ECDSA(hash_alg)                                 \
+    (PSA_ALG_ECDSA_BASE | ((hash_alg) & PSA_ALG_HASH_MASK))
 
 #define PSA_KEY_TYPE_NONE ((psa_key_type_t) 0x0000)
 #define PSA_KEY_TYPE_ECC_KEY_PAIR_BASE ((psa_key_type_t) 0x7100)
