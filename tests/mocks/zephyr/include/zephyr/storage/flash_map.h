6937739ba6b8d98fff25100e75645781249ccf3d
Joel Guittet <joelguittet@gmail.com>
2026-08-30 22:38:17 +0200

    platform: storage: zephyr: nvs: allow to choose storage partition

--- tests/mocks/zephyr/include/zephyr/storage/flash_map.h
+++ tests/mocks/zephyr/include/zephyr/storage/flash_map.h
@@ -1,6 +1,9 @@
 #ifndef __FLASH_MAP_H__
 #define __FLASH_MAP_H__
 
+#define PARTITION_NODE_DEVICE(prop) NULL
+#define PARTITION_NODE_OFFSET(prop) 0
+
 #define PARTITION_DEVICE(label) NULL
 #define PARTITION_OFFSET(label) 0
 #define PARTITION_SIZE(label)   0
